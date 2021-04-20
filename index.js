e147e4da5247ef5305ec370aed17653458ebfbf7
demirbileku <udemirbilek2@gmail.com>
2021-04-20 17:11:12 +0300

    commit tree a14724892b8192be9290c0e6622b5bcb7c0eae44

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector("#main").remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Deniz is the champion!"
+//document.body.appendChild("h1#victory");
+//const newHeader = document.querySelector("h1#victory");
